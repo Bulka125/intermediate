eef13bca9fc191f6885924904fbe1a76104087a6
Bulka125 <138114146+Bulka125@users.noreply.github.com>
2023-08-14 17:51:56 +0400

    Add files via upload

---
+++ промежуточная атестацыя/64/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение N: ");
+        int N = int.Parse(Console.ReadLine());
+
+        PrintNumbers(N);
+
+        Console.ReadKey();
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n < 1)
+            return;
+
+        Console.Write(n + " ");
+
+        PrintNumbers(n - 1);
+    }
+}
---
+++ промежуточная атестацыя/66/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение M: ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите значение N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = M; i <= N; i++)
+            {
+                if (i > 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine("Сумма натуральных элементов в промежутке от {0} до {1} равна {2}.", M, N, sum);
+
+            Console.ReadLine();
+        }
+    }
+}
